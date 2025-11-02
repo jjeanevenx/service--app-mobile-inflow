07007848ed8970c90ae39135fcf2887b8e23e128
Jean Evenx Joseph <jeanevenx@gmail.com>
2025-11-01 22:14:45 -0300

    feat: exportar todas as funções

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,18 +1,8 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
 import {setGlobalOptions} from "firebase-functions";
-import {logger} from "../src/utils/logger";
 
 setGlobalOptions({ maxInstances: 10 });
 
-logger.info("functions ...");
 export { onUserCreate } from "./triggers/newUserTrigger";
 export { onUserInterestChange } from "./triggers/userInterestTrigger";
-
+export { onUserGoalsChange } from "./triggers/userGoalsTrigger";
+export { scheduledNewsDiscovery } from "./triggers/scheduledNewsTrigger";
